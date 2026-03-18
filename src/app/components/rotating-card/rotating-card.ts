90f96c0b098c0b987ae2791f1d9135b45a7d5a56
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-18 14:41:31 +0100

    correction exercice rotating card

--- src/app/components/rotating-card/rotating-card.ts
+++ src/app/components/rotating-card/rotating-card.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rotating-card',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './rotating-card.html',
   styleUrl: './rotating-card.css',
 })
 export class RotatingCard {
-
+  // état de notre interface
+  name = 'sellaouti';
+  firstname = 'aymen';
+  job = 'Formateur';
+  path = 'rotating_card_profile3.png';
 }
